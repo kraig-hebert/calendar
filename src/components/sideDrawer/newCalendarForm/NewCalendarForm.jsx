de8e46caf0ba95e2ba5cc16db08aa339219f8cc0
Kraig Hebert <kraighebert@gmail.com>
2022-11-08 06:38:38 -0500

    updated newCalendar with filter divs, need to add styles

--- src/components/sideDrawer/newCalendarForm/NewCalendarForm.jsx
+++ src/components/sideDrawer/newCalendarForm/NewCalendarForm.jsx
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useStyles } from './styles';
 import PropTypes from 'prop-types';
 
 const NewCalendarForm = (props) => {
   const { calendarFormVisible } = props;
+  const [inputValue, setInputValue] = useState('');
   const setCalendarFormVisible = () => {
-    if (calendarFormVisible) return '175px';
+    if (calendarFormVisible) return '200px';
     else return '0';
   };
   const classes = useStyles({ height: setCalendarFormVisible() });
+
+  const colors = [
+    'black',
+    'yellow',
+    'red',
+    'blue',
+    'pink',
+    'brown',
+    'green',
+    'orange',
+  ];
+
+  const renderedFilters = colors.map((color, index) => (
+    <div key={index} className={classes.filter}></div>
+  ));
+  console.log(renderedFilters);
   return (
     <div className={classes.newCalendarForm}>
-      <p>Add New Calendar</p>
+      <h2>Add New Calendar</h2>
+      <input
+        type="text"
+        name="title"
+        id="title"
+        value={inputValue}
+        placeholder="Title"
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <p>Select Filter Color:</p>
+      <div className={classes.filtersContainer}>{renderedFilters}</div>
     </div>
   );
 };
