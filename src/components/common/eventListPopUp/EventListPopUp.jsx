ff22a0b168631f3d361026d966ea7d05bbbc606c
Kraig Hebert <kraighebert@gmail.com>
2022-12-08 07:16:55 -0500

    started to build eventListPopups

---
+++ src/components/common/eventListPopUp/EventListPopUp.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import { useStyles } from './styles';
+
+const EventListPopUp = () => {
+  const classes = useStyles();
+  return <div>EventListPopUp</div>;
+};
+
+export default EventListPopUp;
